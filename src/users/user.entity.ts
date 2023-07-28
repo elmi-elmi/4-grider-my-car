4f7156b477d60beaaa0472f3401c65ef53923f74
elmi.a <shahrokhelmi@gmail.com>
2023-07-28 21:10:55 +0330

    user service completed

--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -1,5 +1,5 @@
 import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
-
+import { AfterInsert, AfterRemove, AfterUpdate } from 'typeorm';
 @Entity()
 export class User {
   @PrimaryGeneratedColumn()
@@ -10,4 +10,19 @@ export class User {
 
   @Column()
   password: string;
+
+  @AfterInsert()
+  logInsert() {
+    console.log('This id inserted: ', this.id);
+  }
+
+  @AfterUpdate()
+  logUpdate() {
+    console.log('This id update: ', this.id);
+  }
+
+  @AfterRemove()
+  logRemove() {
+    console.log('This id removed: ', this.id);
+  }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,50 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './user.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(@InjectRepository(User) private repo: Repository<User>) {}
+
+  create(email: string, password: string) {
+    const user = this.repo.create({ email, password });
+    return this.repo.save(user);
+  }
+
+  findOne(id: number) {
+    const user = this.repo.findOne({
+      where: {
+        id,
+      },
+    });
+    return user;
+  }
+
+  async find(email: string) {
+    const users = await this.repo.find({
+      where: {
+        email,
+      },
+    });
+    console.log(users);
+    return users;
+  }
+
+  async update(id: number, attrs: Partial<User>) {
+    const user = await this.findOne(id);
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+    Object.assign(user, attrs);
+    return this.repo.save(user);
+  }
+
+  async remove(id: number) {
+    const user = await this.findOne(id);
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+    return this.repo.remove(user);
+  }
+}
